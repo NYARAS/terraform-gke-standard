995e8bc0975891734303bb82a737dcd34fbb5cd8
Calvine Otieno <nyarangaotieno@gmail.com>
2022-04-27 22:54:59 +0300

    feat: add terraform provider and variables for gke
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "gcp_credentials" {
+    type = string
+    description = "Location of the service account for GCP."
+}
+
+variable "gcp_project_id" {
+  type = string
+  description = "GCP project id to create the resources."
+}
+
+variable "gcp_region" {
+  type = string
+  description = "The GCP region to create the resources."
+  default = "us-west1"
+}
