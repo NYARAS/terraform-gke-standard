54f6778e7a85a6c0aba3c36fbd7cec7a0cf0fbbe
Calvine Otieno <nyarangaotieno@gmail.com>
2022-04-28 13:03:36 +0300

    chore: use variables for resource values
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- variables.tf
+++ variables.tf
@@ -11,5 +11,30 @@ variable "gcp_project_id" {
 variable "gcp_region" {
   type        = string
   description = "The GCP region to create the resources."
-  default     = "us-west1"
+  default     = "us-central1"
+}
+variable "cluster_name" {
+  type        = string
+  description = "The name of the cluster, unique within the project and location."
+  default     = "primary"
+}
+variable "master_ipv4_cidr_block" {
+  type        = string
+  description = "The IP range in CIDR notation to use for the hosted master network."
+  default     = "172.16.0.0/28"
+}
+variable "router_nat_name" {
+  type        = string
+  description = "Name of the NAT service."
+  default     = "nat"
+}
+variable "compute_address_name" {
+  type        = string
+  description = " Name of the resource."
+  default     = "nat"
+}
+variable "service_account_name" {
+  type        = string
+  description = "Name of the resource."
+  default     = "kubernetes"
 }
